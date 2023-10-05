0ad40f288252f257dd78db3880f37450e873476a
Omer Can Bozkurt <omerbozkurt912@gmail.com>
2023-10-05 22:16:42 +0300

    loops

---
+++ loops/for.py
@@ -0,0 +1,50 @@
+numbers=[0,2,4,1,5]
+
+for a in numbers:
+    print(a)
+
+names=['ali','ayse','hasan']
+
+for name in names:
+    print(name)
+
+name='ali yilmaz'
+
+for n in name:
+    print(n)
+
+tuple=1,2,4,5,9
+
+for t in tuple:
+    print(t)
+
+tuple=((1,3),(3,2),(3,5),(8,3))
+
+for a in tuple:
+    print(a)
+
+for a,b in tuple:
+    print(a)
+
+for a,b in tuple:
+    print(b)
+
+for a,b in tuple:
+    print(a,b)
+
+d={'k1':'ali','k2':'ayse','k3':'zeynep'}
+
+for item in d:
+    print(item)
+
+for item in d.items():
+    print(item)
+
+for key,item in d.items():
+    print(key)
+
+for key,item in d.items():
+    print(item)
+
+for key,item in d.items():
+    print(key,item)
---
+++ loops/forDemo.py
@@ -0,0 +1,42 @@
+numbers=[1,3,4,6,9,7,8,12,19,21]
+
+#q1 which numbers in the list of numbers are exactly divisible by 3
+
+for number in numbers:
+    if number%3==0:
+        print(number)
+
+#q2 what is the sum of the numbers in the list of numbers
+
+sum=0
+for number in numbers:
+    sum=sum+number
+print(sum)
+
+#q3 square the odd numbers in the list of numbers
+
+for number in numbers:
+    if number%2==1:
+        print(number**2)
+
+cities=['kocaeli','istanbul','ankara','izmir','rize']
+
+#q4 which of the cities has at most five characters
+
+for city in cities:
+    if len(city)<=5:
+        print(city)
+
+urunler=[{'name':'samsung s6','price':'3000'},
+         {'name':'samsung s7','price':'4000'},
+         {'name':'samsung s8','price':'5000'},
+         {'name':'samsung s9','price':'6000'}
+         ]
+
+#q5 what is the sum of the prices of the products
+
+for urun in urunler:
+    for name,price in urun.items():
+        print(price)
+
+#q6 products with a maximum price of 5000
